1458bf075f1872eb5cfa74d4d398eb0652fb02d4
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-06-01 19:46:44 -0300

    perf: deletando arquivos por hora desnessário

--- prisma/migrations/20230523222947_edit_people/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `people` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "people" DROP COLUMN "status";
