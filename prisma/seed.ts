b834962ade6abc104d4f982e110cfab656fadad1
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-06-01 19:49:07 -0300

    feat(database): novos relacionamentos e migrates, e seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,30 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const admRole = {
-  status: "active",
-  name: "ADMIN",
-}
+const admRole = [
+  {
+    status: 'ativo',
+    name: 'admin',
+  },
+  {
+    status: 'ativo',
+    name: 'dentista',
+  },
+];
+
 async function run() {
-  await prisma.role.create(
-    {
-      data: admRole
-    }
-  )
+  await prisma.role.createMany({
+    data: admRole,
+  });
 }
 
 run()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
