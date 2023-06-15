133b00390bce46273024760d111dd4647745a97e
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-06-15 20:39:39 -0300

    feat(Validação):  Criando validação de dados para um create de Employee

--- api/Employees/Middlewares/CreateEmployeeMiddleware.ts
+++ api/Employees/Middlewares/CreateEmployeeMiddleware.ts
@@ -1,11 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response, response } from 'express';
 import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
+import  { isCPF, isPIS }  from 'validation-br'
 
 import { BadRequestError } from 'api/Shared/Utils/Error/ApiErrors';
 import GeneratorErrorResponse from 'api/Shared/Utils/Error/Helpers/GeneratorErrorMessages';
 import dayjs from 'dayjs';
 import { prismaConnection } from '@prisma/PrismaConnection';
+import findCEP from 'api/Shared/Services/CEP/FindCEP';
 
 export default async function CreateEmployeeMiddleware(
   request: Request,
@@ -21,6 +23,8 @@ const verifyMiddlewares = async (
   next: NextFunction,
 ) => {
   const regexName = /^[a-zA-ZÀ-ÖØ-öø-ÿ ]+$/u;
+  const regexRG = /^[a-zA-Z0-9]+$/gm;
+  const regexTelephone = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/gm;
   const createEmployeeSchema = z.object({
     name: z
       .string(
@@ -36,9 +40,12 @@ const verifyMiddlewares = async (
         ),
       )
       .datetime(),
-    rg: z.string(
-      GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired('rg'),
-    ),
+    rg: z
+      .string(
+        GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired('rg'),
+      )
+      .regex(regexRG, 'RG só pode conter números ou letras')
+      .max(15, 'RG não pode ter mais de 15 caracteres'),
     cpf: z.string(
       GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired('cpf'),
     ),
@@ -85,11 +92,14 @@ const verifyMiddlewares = async (
           'city',
         ),
       ),
-      postalCode: z.string(
-        GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired(
-          'postalCode',
-        ),
-      ),
+      postalCode: z
+        .string(
+          GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired(
+            'postalCode',
+          ),
+        )
+        .min(8, 'CPF deve ter 8 caracteres.')
+        .max(8, 'CPF deve ter 8 caracteres.'),
       state: z.string(
         GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired(
           'state',
@@ -97,11 +107,13 @@ const verifyMiddlewares = async (
       ),
     }),
     telephone: z.object({
-      number: z.string(
-        GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired(
-          'telephone',
-        ),
-      ),
+      number: z
+        .string(
+          GeneratorErrorResponse.generateErrorMessageInTypeStringOrRequired(
+            'telephone',
+          ),
+        )
+        .regex(regexTelephone, 'Telefone inválido.'),
     }),
   });
 
@@ -113,40 +125,48 @@ const verifyMiddlewares = async (
       GeneratorErrorResponse.messageResponseError(message),
     );
   }
+
+  const { cpf } = isParseSuccess.data;
+  verifyCPF(cpf);
+
   const { birth_date } = isParseSuccess.data;
   verifyBirthDate(birth_date);
 
   const { hire_date } = isParseSuccess.data;
   verifyHireDate(hire_date, birth_date);
 
+  const { pis_pasep} = isParseSuccess.data;
+  verifyPisPasep(pis_pasep);
+
   const { role_id } = isParseSuccess.data;
   await verifyRoleExist(role_id);
+
+  const { postalCode } = isParseSuccess.data.address;
+  await verifyCEP(postalCode);
+
 
   next();
 };
 
-const verifyHireDate = (
-  hire_date: string,
-  birth_date: string,
-) => {
+const verifyHireDate = (hire_date: string, birth_date: string) => {
   const now = dayjs();
   const hire_dateDayjs = dayjs(hire_date);
   const hireDateIsAfterBirth = hire_dateDayjs.isAfter(birth_date);
 
   if (!hireDateIsAfterBirth) {
-    throw new BadRequestError('Data de contração não pode ser antes da data de aniversário');
+    throw new BadRequestError(
+      'Data de contração não pode ser antes da data de aniversário',
+    );
   }
 
   const hireDateIsBeforeNow = hire_dateDayjs.isBefore(now);
 
-  if(!hireDateIsBeforeNow) {
-    throw new BadRequestError('Data de contratação não pode ser no futuro')
+  if (!hireDateIsBeforeNow) {
+    throw new BadRequestError('Data de contratação não pode ser no futuro');
   }
 };
 
-const verifyBirthDate = (
-  birth_date: string,
-) => {
+const verifyBirthDate = (birth_date: string) => {
   const now = dayjs();
   const birth_dateDayjs = dayjs(birth_date);
   const birthdayDateIsInTheFuture = birth_dateDayjs.isBefore(now);
@@ -157,7 +177,24 @@ const verifyBirthDate = (
 };
 
 const verifyRoleExist = async (role_id: number) => {
-  const role = await prismaConnection.role.findUnique({where: {id: role_id}})
+  const role = await prismaConnection.role.findUnique({
+    where: { id: role_id },
+  });
 
-  if(!role) throw new BadRequestError("Cargo não encontrado.")
+  if (!role) throw new BadRequestError('Cargo não encontrado.');
+};
+
+const verifyCEP = async (cep: string) => {
+  const ceps = await findCEP.get(`${cep}/json`).catch(() => {
+    throw new BadRequestError("CEP inválido");
+  });
+};
+
+const verifyCPF = (cpf: string) => {
+  if(!isCPF(cpf)) throw new BadRequestError("CPF inválido")
 }
+
+const verifyPisPasep =(pis_pasep: string) => {
+  if(!isPIS(pis_pasep)) throw new BadRequestError("Pis/PASEP inválido")
+}
+//TODO alterar quantidade de caracteres de RG
---
+++ api/Shared/Utils/Types/validation-br.d.ts
@@ -0,0 +1,1 @@
+declare module 'validation-br';
