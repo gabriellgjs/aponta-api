fbf32c402b5d76195c679a864e5e871c6de6e841
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-06-20 21:47:21 -0300

    fix(CEP) Resolvendo erros de retorno da api utilizada

--- api/Shared/Middlewares/CepValidator.ts
+++ api/Shared/Middlewares/CepValidator.ts
@@ -2,7 +2,9 @@ import findCEP from "../Services/CEP/FindCEP";
 import { BadRequestError } from "../Utils/Error/ApiErrors";
 
 export default async function (cep: string){
-  await findCEP.get(`${cep}/json`).catch(() => {
+  await findCEP.get(`${cep}/json`).then((response) => {
+    if(response.data?.erro) throw new BadRequestError("CEP inválido");
+  }).catch(() => {
     throw new BadRequestError("CEP inválido");
   });
 };
